07a2a6aaa77724ebbe2689d6d75c919c4f1997dc
Cool Lime <mz.zhoumeng@gmail.com>
2018-12-03 03:11:30 -0800

    Search with TicketMaster API
    The API successfully responses to requests with latitude and longitude data

--- BoredomMaster/src/external/TicketMasterAPI.java
+++ BoredomMaster/src/external/TicketMasterAPI.java
@@ -1,5 +1,11 @@
 package external;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+import java.net.URL;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -10,7 +16,52 @@ public class TicketMasterAPI {
 	
 	// Search based on location
 	public JSONArray search(double lat, double lon, String keyword) {
-        return null;
+		if (keyword == null) {
+			keyword = DEFAULT_KEYWORD;
+		}
+		
+		try {
+			// Specifically use UTF-8
+			// e.g. spaces in the url will be converted to "20%"
+			keyword = URLEncoder.encode(keyword, "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		String geoHash = GeoHash.encodeGeohash(lat, lon, 9);
+		
+		String query = String.format("apikey=%s&geoPoint=%s&keyword=%s&radius=50", API_KEY, geoHash, keyword);
+
+	    
+		try {
+			// Create a connection to the remote object referred to by the url in order to perform basic HTTP requests 
+			HttpURLConnection connection = (HttpURLConnection) new URL(URL + "?" + query).openConnection();
+			connection.setRequestMethod("GET");
+			
+			// Log the response code for debugging purpose
+			int responseCode = connection.getResponseCode();
+			System.out.println("Sending 'GET' request to URL: " + URL);
+			System.out.println("Response Code: " + responseCode);
+		
+			// Get the full response body
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			StringBuilder response = new StringBuilder();
+			
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			
+			JSONObject obj = new JSONObject(response.toString());
+			if (!obj.isNull("_embedded")) {
+				JSONObject embbeded = obj.getJSONObject("_embedded");
+				return embbeded.getJSONArray("events");
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new JSONArray();	
 	}
 
 	// Test the response from the API
@@ -25,6 +76,19 @@ public class TicketMasterAPI {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Test sample TicketMaster API requests
+	 */
+	public static void main(String[] args) {
+		TicketMasterAPI api = new TicketMasterAPI();
+		// San Diego Zoo
+		api.queryAPI(32.7353, 117.1490);
+		// SDCCU Stadium
+		api.queryAPI(32.7831, 117.1196);
+		// Old Trafford Stadium
+		api.queryAPI(53.4631, 2.2913);
 	}
 
 }
